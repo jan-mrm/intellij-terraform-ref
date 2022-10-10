9fa79c86ff0c717eb3ed5d95eea03bfd0a5e9169
jan-mrm <67435696+jan-mrm@users.noreply.github.com>
2022-10-10 19:05:01 +0200

    warning setup

--- main/main.tf
+++ main/main.tf
@@ -1,3 +1,11 @@
-module "m" {
-  source = "../modules/some-module"
+module "m1" {
+  # working without any warning
+  source = "git@github.com:jan-mrm/intellij-terraform-ref.git//modules/some-module?ref=some-module%2F1.0.0"
+  file-name-suffix = "m1"
+}
+
+module "m2" {
+  # working, but marked with warning "Cannot locate module locally: Unknown reason"
+  source = "git@github.com:jan-mrm/intellij-terraform-ref.git//modules/some-module?ref=some-module/1.0.0"
+  file-name-suffix = "m2"
 }
